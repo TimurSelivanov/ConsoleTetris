d1042cefa1480b797677f87041d9f51ff84afaf7
TimSelivan <timselivanson@gmail.com>
2023-03-04 14:57:14 +0300

    Add draft of the fields and methods to Tetris.java with commnets

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ConsoleTetris
 
-Simple app to try the code from stackoverflow
+This simple game application is working on the console.
+
+It has been created to test the code that allows to observe events from the keyboard.
--- src/main/java/com/consoletetris/model/Tetris.java
+++ src/main/java/com/consoletetris/model/Tetris.java
@@ -2,7 +2,29 @@ package com.consoletetris.model;
 
 //the game class with main method
 public class Tetris {
-    public static void main(String[] args) {
 
+    private Field field;
+    private Figure figure;
+    static Tetris game;
+
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void run() {
+        //manage all game process
+    }
+
+    public void step() {
+        //manage the on step of the game
+    }
+    public static void main(String[] args) {
+        game = new Tetris(); //game object
+        game.run(); //starts the game
     }
 }
