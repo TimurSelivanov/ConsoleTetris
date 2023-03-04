5b789d01ea593ce2f961acf02a8a60a8fdb69fb2
TimSelivan <timselivanson@gmail.com>
2023-03-04 15:04:07 +0300

    Add draft of the fields and methods to Field.java.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ConsoleTetris
 
-This simple game application is working on the console.
+This simple game application works on the console.
 
 It has been created to test the code that allows to observe events from the keyboard.
--- src/main/java/com/consoletetris/model/Field.java
+++ src/main/java/com/consoletetris/model/Field.java
@@ -1,4 +1,41 @@
 package com.consoletetris.model;
 
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Field(int width, int height) {
+        this.height = height;
+        this.width = width;
+        matrix = new int[height][width];
+    }
+
+    public void print() {
+
+    }
+
+    public void removeFullLines() {
+
+    }
+
+    public Integer getValue(int x, int y) {
+        return null;
+    }
+
+    public void setValue(int x, int y, int value) {
+
+    }
 }
