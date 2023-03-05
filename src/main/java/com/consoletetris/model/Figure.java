a688d453a87d1a386d6a0f86198ef94ec8c7e2c0
TimSelivan <timselivanson@gmail.com>
2023-03-05 16:56:08 +0300

    Implement Figure.rotate

--- src/main/java/com/consoletetris/model/Figure.java
+++ src/main/java/com/consoletetris/model/Figure.java
@@ -44,7 +44,16 @@ public class Figure {
     }
 
     public void rotate() {
+        //temp array
+        int[][] matrix2 = new int[3][3];
+        //180 degrees
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix2[i][j] = matrix[j][i];
+            }
+        }
 
+        matrix = matrix2;
     }
 
     public void downMaximum() {
