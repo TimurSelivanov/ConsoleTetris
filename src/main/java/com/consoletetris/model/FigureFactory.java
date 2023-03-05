639c0761fab7c04de995087909bda711b4fd6ba8
TimSelivan <timselivanson@gmail.com>
2023-03-05 17:00:38 +0300

    Add bricks to FigureFactory

--- src/main/java/com/consoletetris/model/FigureFactory.java
+++ src/main/java/com/consoletetris/model/FigureFactory.java
@@ -1,6 +1,29 @@
 package com.consoletetris.model;
 
 public class FigureFactory {
+
+    public static final int[][][] BRICKS = {{
+            {1, 1, 0},                          //   X X
+            {0, 1, 1},                          //     X X
+            {0, 0, 0}}, {                       //
+
+            {1, 0, 0},                          //   X
+            {1, 1, 0},                          //   X X
+            {0, 1, 0}}, {                       //     X
+
+            {0, 1, 0},                          //   X
+            {0, 1, 0},                          //   X
+            {0, 1, 0}}, {                       //   X
+
+            {1, 1, 0},                          //   X X
+            {1, 1, 0},                          //   X X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {0, 1, 0},                          //     X
+            {0, 0, 0}},                         //
+    };
+
     public static Figure createRandomFigure(int x, int y) {
         return  null;
     }
